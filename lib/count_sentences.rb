1280dac72d017924e2530b5df3d3c77ee1c3da25
kenkuts <kennethchoi19@gmail.com>
2020-04-12 19:24:38 +0300

    commit

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,21 @@ require 'pry'
 class String
 
   def sentence?
-    
+    # self[self.length - 1] == "." ? true : false
+    self.end_with?(".")
   end
 
   def question?
-
+    # self[self.length - 1] == "?" ? true : false
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    # self[self.length - 1] == "!" ? true : false
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    self.split(". ").map{|ele| ele.split("! ")}.flatten.map{|ele| ele.split("? ")}.flatten.count
   end
 end
